23680bf71eb83ccff3ef8317b8d9bb123921155f
Hashyy <lodej.jakub@gmail.com>
2025-10-26 22:58:29 +0100

    Fix main

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,7 @@
 #include "Frontend/Token.hpp"
 
 int main() {
-    auto tokens = { Token(5), Token(Token::Type::BinaryPlus), Token(3.5) };
-    for(auto& token : tokens) {
-        std::cout << token << std::endl;
-    }
+    
 
     return 0;
 }
